b503e4f0d874376a123be2957f7fad619962dbfe
Sergiu Frunza <sergiufrunza@Sergius-MBP.cable.rcn.com>
2021-06-29 14:36:54 -0400

    keys

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,14 +5,23 @@ import "firebase/auth";
 import "firebase/storage";
 import axios from "axios";
 
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APPID,
+//   measurementId: process.env.REACT_APP_MEASUREMENT_ID
+// };
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: "AIzaSyD8kaW7t7JOWBaBziG8AWvs8_YQwN_jzUA",
+  authDomain: "tailormade-c00e1.firebaseapp.com",
+  projectId: "tailormade-c00e1",
+  storageBucket: "tailormade-c00e1.appspot.com",
+  messagingSenderId: "823336005326",
+  appId: "1:823336005326:web:c4ce20911f3e9d31016a9f",
+  measurementId: "G-7TXDXNVWYD",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
